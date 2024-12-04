1c5d2a781f46375f6d849aea8086c37c57e0ae3d
Jerald Bianan <jeraldbianan@gmail.com>
2024-12-05 07:17:23 +0800

    added reusable ui components

---
+++ components/ui/ButtonComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-btn
+    v-bind="attrs"
+    square
+    unelevated
+    color="primary"
+    class="text-capitalize"
+  >
+    <slot />
+  </q-btn>
+</template>
+
+<script setup>
+import { useAttrs } from "vue";
+
+const attrs = useAttrs();
+</script>
---
+++ components/ui/InputField.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <label :for="attrs.id || ''">{{ label }}</label>
+    <q-input v-bind="attrs" stack-label square dense outlined :model-value="modelValue" @update:model-value="updateModelValue">
+      <slot />
+    </q-input>
+  </div>
+</template>
+
+<script setup>
+import { useAttrs } from "vue";
+
+const props = defineProps({
+  label: {
+    type: String,
+    default: "",
+  },
+  defaultValue: { type: [String, Number], required: false },
+  modelValue: { type: [String, Number], required: false },
+  class: { type: null, required: false },
+});
+
+const emits = defineEmits(["update:modelValue"]);
+
+const attrs = useAttrs();
+
+const updateModelValue = (value) => {
+  emits("update:modelValue", value);
+};
+</script>
