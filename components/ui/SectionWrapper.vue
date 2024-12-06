ef3885aed25883eb815f1417459e114bf83fdae2
Jerald Bianan <jeraldbianan@gmail.com>
2024-12-06 23:40:02 +0800

    created a reusable SectionWrapper component

---
+++ components/ui/SectionWrapper.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="container">
+    <div v-if="$slots.header">
+      <slot name="header"></slot>
+    </div>
+
+    <slot></slot>
+  </section>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.container {
+  padding: 25px 47px 48px;
+  background-color: #fff;
+  margin-top: 59px;
+}
+</style>
