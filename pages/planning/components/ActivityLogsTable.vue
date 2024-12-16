3f572f99d62248d2b9cb134a8b071819a5dc4a2f
Jerald Bianan <jeraldbianan@gmail.com>
2024-12-17 02:42:50 +0800

    refactor ActivityLogsTable and implement the rows and columns inside it

--- pages/planning/components/ActivityLogsTable.vue
+++ pages/planning/components/ActivityLogsTable.vue
@@ -23,7 +23,43 @@
 <script setup>
 import { ref, onMounted } from "vue";
 
-import { columns, originalRows } from "../../../data/activityLogsDummyData";
+const columns = ref([
+  {
+    name: "action",
+    required: true,
+    label: "Action",
+    align: "left",
+    field: (row) => row.action,
+    format: (val) => `${val}`,
+    sortable: true,
+  },
+  {
+    name: "personnel",
+    required: true,
+    label: "Personnel",
+    align: "left",
+    field: (row) => row.personnel,
+    format: (val) => `${val}`,
+    sortable: true,
+  },
+  {
+    name: "date_time",
+    required: true,
+    label: "Date & Time",
+    align: "left",
+    field: (row) => row.date_time,
+    format: (val) => `${val}`,
+    sortable: true,
+  },
+]);
+
+const originalRows = ref([
+  {
+    action: "Performed",
+    personnel: "Ethan Blake",
+    date_time: "August 10, 2024 8:00 AM",
+  },
+]);
 
 const rows = ref([]);
 const tableRef = ref();
