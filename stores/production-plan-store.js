dc53e974d49ad1858e851482561aea98abfe5de0
Jerald Bianan <jeraldbianan@gmail.com>
2024-12-19 07:38:48 +0800

    created store production-plan and define actions GetProductionPlans, GetProductionPlan, and SearchProductionPlans

---
+++ stores/production-plan-store.js
@@ -0,0 +1,50 @@
+import { defineStore } from "pinia";
+import { api } from "boot/axios";
+
+export const useProductionPlanStore = defineStore("production-plan", {
+  state: () => ({}),
+  actions: {
+    GetProductionPlans(request) {
+      return new Promise((resolve, reject) => {
+        api
+          .get(
+            `cig-production/production-plan/production-planning?offset=${request.offset}`
+          )
+          .then((response) => {
+            resolve(response.data);
+          })
+          .catch((response) => {
+            reject(response.data);
+          });
+      });
+    },
+
+    GetProductionPlan(id) {
+      return new Promise((resolve, reject) => {
+        api
+          .get(`cig-production/production-plan/production-planning/${id}`)
+          .then((response) => {
+            resolve(response.data);
+          })
+          .catch((response) => {
+            reject(response.data);
+          });
+      });
+    },
+
+    SearchProductionPlans(request) {
+      return new Promise((resolve, reject) => {
+        api
+          .get(
+            `cig-production/production-plan/production-planning?search_keyword=${request.keyword}`
+          )
+          .then((response) => {
+            resolve(response.data);
+          })
+          .catch((response) => {
+            reject(response.data);
+          });
+      });
+    },
+  },
+});
