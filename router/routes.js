c1c1e66f90b9cbc389cb140a79443ed3373726aa
Jerald Bianan <jeraldbianan@gmail.com>
2024-12-27 01:42:08 +0800

    updated routes structure

--- router/routes.js
+++ router/routes.js
@@ -10,25 +10,34 @@ const routes = [
         component: () => import("../pages/IndexPage.vue"),
       },
       {
-        path: "planning/view-production-plans",
-        name: "viewProductionPlans",
-        component: () => import("../pages/planning/ViewProductionPlans.vue"),
-      },
-      {
-        path: "planning/view-production-plans/:id",
-        name: "viewProductionPlanDetails",
-        component: () =>
-          import("../pages/planning/ViewProductionPlanDetails.vue"),
-      },
-      {
-        path: "planning/add-production-plan",
-        name: "addProductionPlan",
-        component: () => import("../pages/planning/AddProductionPlan.vue"),
-      },
-      {
-        path: "planning/edit-production-plan/:id",
-        name: "editProductionPlan",
-        component: () => import("../pages/planning/EditProductionPlan.vue"),
+        path: "planning",
+        name: "Planning",
+        component: () => import("../pages/planning/IndexPage.vue"),
+        redirect: "../pages/planning/ViewProductionPlans.vue",
+        children: [
+          {
+            path: "view-production-plans",
+            name: "viewProductionPlans",
+            component: () =>
+              import("../pages/planning/ViewProductionPlans.vue"),
+          },
+          {
+            path: "view-production-plans/:id",
+            name: "viewProductionPlanDetails",
+            component: () =>
+              import("../pages/planning/ViewProductionPlanDetails.vue"),
+          },
+          {
+            path: "add-production-plan",
+            name: "addProductionPlan",
+            component: () => import("../pages/planning/AddProductionPlan.vue"),
+          },
+          {
+            path: "edit-production-plan/:id",
+            name: "editProductionPlan",
+            component: () => import("../pages/planning/EditProductionPlan.vue"),
+          },
+        ],
       },
 
       // Add your route here
