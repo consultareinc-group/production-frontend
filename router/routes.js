f2f3844c763abcc316ea1a03ba4b7f608a36dcea
Jerald Bianan <jeraldbianan@gmail.com>
2025-01-10 03:31:13 +0800

    setup routes

--- router/routes.js
+++ router/routes.js
@@ -4,8 +4,48 @@ const routes = [
     name: "Production Management System",
     component: () => import("../layouts/MainLayout.vue"),
     children: [
-      { path: "", name: "Home", component: () => import("../pages/IndexPage.vue") },
-      { path: "menu-item", name: "menu-item", component: () => import("../pages/IndexPage.vue") },
+      {
+        path: "",
+        name: "Home",
+        component: () => import("../pages/IndexPage.vue"),
+      },
+      {
+        path: "processing",
+        name: "processing",
+        redirect: { name: "view-processes" },
+        children: [
+          {
+            path: "view",
+            name: "view-processes",
+            component: () => import("../pages/processing/ViewProcesses.vue"),
+          },
+          // {
+          //   path: "view/:id",
+          //   name: "view-pre-operations-verification-details",
+          //   component: () =>
+          //     import(
+          //       "../pages/pre-operations-verification/ViewPreOperationsVerificationDetails.vue"
+          //     ),
+          // },
+          // {
+          //   path: "add",
+          //   name: "add-pre-operations-verification",
+          //   component: () =>
+          //     import(
+          //       "../pages/pre-operations-verification/AddPreOperationsVerification.vue"
+          //     ),
+          // },
+          // {
+          //   path: "edit/:id",
+          //   name: "edit-pre-operations-verification",
+          //   component: () =>
+          //     import(
+          //       "../pages/pre-operations-verification/EditPreOperationsVerification.vue"
+          //     ),
+          // },
+        ],
+      },
+
       // Add your route here
     ],
   },
