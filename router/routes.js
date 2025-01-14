6df6791ba44ba95b396a6183982f433d2f790671
Jerald Bianan <jeraldbianan@gmail.com>
2025-01-15 05:27:52 +0800

    develop the view weigh out and its necessary functionalities

--- router/routes.js
+++ router/routes.js
@@ -4,8 +4,40 @@ const routes = [
     name: "Production Management System",
     component: () => import("../layouts/MainLayout.vue"),
     children: [
-      { path: "", name: "Home", component: () => import("../pages/IndexPage.vue") },
-      { path: "menu-item", name: "menu-item", component: () => import("../pages/IndexPage.vue") },
+      {
+        path: "",
+        name: "Home",
+        component: () => import("../pages/IndexPage.vue"),
+      },
+      {
+        path: "weigh-out",
+        name: "weigh-out",
+        redirect: { name: "view-weigh-out" },
+        children: [
+          {
+            path: "view",
+            name: "view-weigh-out",
+            component: () => import("../pages/weigh-out/ViewWeighOut.vue"),
+          },
+          // {
+          //   path: "view/:id",
+          //   name: "view-equipment-details",
+          //   component: () =>
+          //     import("../pages/equipment/ViewEquipmentDetails.vue"),
+          // },
+          // {
+          //   path: "add",
+          //   name: "add-equipment",
+          //   component: () => import("../pages/equipment/AddEquipment.vue"),
+          // },
+          // {
+          //   path: "edit/:id",
+          //   name: "edit-equipment",
+          //   component: () => import("../pages/equipment/EditEquipment.vue"),
+          // },
+        ],
+      },
+
       // Add your route here
     ],
   },
