dcfa635ba1ac64b987280f789f1d1e37855800a2
Jerald Bianan <jeraldbianan@gmail.com>
2025-02-05 12:19:46 +0800

    setup routes and develop the ViewPostProductionVerification page and its necessary UI functionalities

--- router/routes.js
+++ router/routes.js
@@ -4,8 +4,43 @@ const routes = [
     name: "Production Management System",
     component: () => import("../layouts/MainLayout.vue"),
     children: [
-      { path: "", name: "Home", component: () => import("../pages/IndexPage.vue") },
-      { path: "menu-item", name: "menu-item", component: () => import("../pages/IndexPage.vue") },
+      {
+        path: "",
+        name: "Home",
+        component: () => import("../pages/IndexPage.vue"),
+      },
+      {
+        path: "post-production-verification",
+        name: "post-production-verification",
+        redirect: { name: "view-post-production-verification" },
+        children: [
+          {
+            path: "view",
+            name: "view-post-production-verification",
+            component: () =>
+              import(
+                "../pages/post-production-verification/ViewPostProductionVerification.vue"
+              ),
+          },
+          // {
+          //   path: "view/:id",
+          //   name: "view-weigh-out-details",
+          //   component: () =>
+          //     import("../pages/weigh-out/ViewWeighOutDetails.vue"),
+          // },
+          // {
+          //   path: "add",
+          //   name: "add-weigh-out",
+          //   component: () => import("../pages/weigh-out/AddWeighOut.vue"),
+          // },
+          // {
+          //   path: "edit/:id",
+          //   name: "edit-weigh-out",
+          //   component: () => import("../pages/weigh-out/EditWeighOut.vue"),
+          // },
+        ],
+      },
+
       // Add your route here
     ],
   },
