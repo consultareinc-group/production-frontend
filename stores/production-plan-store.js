077902b5d46c9cf660c8b1fec5865a9fe30e6d3a
Jerald Bianan <jeraldbianan@gmail.com>
2025-03-29 01:39:50 +0800

    correct API Endpoint and added more logic to the SearchProductionPlans

--- stores/production-plan-store.js
+++ stores/production-plan-store.js
@@ -8,7 +8,7 @@ export const useProductionPlanStore = defineStore("production-plan", {
       return new Promise((resolve, reject) => {
         api
           .get(
-            `cig-production/production-plan/production-planning?offset=${request.offset}`
+            `production-management/production-planning?offset=${request.offset}`
           )
           .then((response) => {
             resolve(response.data);
@@ -22,7 +22,7 @@ export const useProductionPlanStore = defineStore("production-plan", {
     GetProductionPlan(id) {
       return new Promise((resolve, reject) => {
         api
-          .get(`cig-production/production-plan/production-planning/${id}`)
+          .get(`production-management/production-planning/${id}`)
           .then((response) => {
             resolve(response.data);
           })
@@ -33,11 +33,28 @@ export const useProductionPlanStore = defineStore("production-plan", {
     },
 
     SearchProductionPlans(request) {
+      const params = {};
+
+      if (request.search_keyword) {
+        params.search_keyword = request.search_keyword;
+      }
+      if (
+        request.status &&
+        request.status.value !== undefined &&
+        request.status.value !== ""
+      ) {
+        params.status = request.status.value;
+      }
+      if (request.start_date_and_time) {
+        params.start_date_and_time = request.start_date_and_time;
+      }
+      if (request.end_date_and_time) {
+        params.end_date_and_time = request.end_date_and_time;
+      }
+
       return new Promise((resolve, reject) => {
         api
-          .get(
-            `cig-production/production-plan/production-planning?search_keyword=${request.keyword}`
-          )
+          .get("production-management/production-planning", { params })
           .then((response) => {
             resolve(response.data);
           })
@@ -47,68 +64,68 @@ export const useProductionPlanStore = defineStore("production-plan", {
       });
     },
 
-    AddProductionPlan(request) {
-      return new Promise((resolve, reject) => {
-        api
-          .post(
-            "cig-production/production-plan/production-planning",
-            request.payload
-          )
-          .then((response) => {
-            resolve(response.data);
-          })
-          .catch((error) => {
-            reject(error.response.data);
-          });
-      });
-    },
+    // AddProductionPlan(request) {
+    //   return new Promise((resolve, reject) => {
+    //     api
+    //       .post(
+    //         "cig-production/production-plan/production-planning",
+    //         request.payload
+    //       )
+    //       .then((response) => {
+    //         resolve(response.data);
+    //       })
+    //       .catch((error) => {
+    //         reject(error.response.data);
+    //       });
+    //   });
+    // },
 
-    EditProductionPlan(request) {
-      return new Promise((resolve, reject) => {
-        api
-          .put(
-            `cig-production/production-plan/production-planning/${request.id}`,
-            request.payload
-          )
-          .then((response) => {
-            resolve(response.data);
-          })
-          .catch((error) => {
-            reject(error.response.data);
-          });
-      });
-    },
+    // EditProductionPlan(request) {
+    //   return new Promise((resolve, reject) => {
+    //     api
+    //       .put(
+    //         `cig-production/production-plan/production-planning/${request.id}`,
+    //         request.payload
+    //       )
+    //       .then((response) => {
+    //         resolve(response.data);
+    //       })
+    //       .catch((error) => {
+    //         reject(error.response.data);
+    //       });
+    //   });
+    // },
 
-    ArchiveProductionPlan(request) {
-      return new Promise((resolve, reject) => {
-        api
-          .put(
-            `cig-production/production-plan/production-planning/archive/${request.id}`,
-            request.payload
-          )
-          .then((response) => {
-            resolve(response.data);
-          })
-          .catch((error) => {
-            reject(error.response.data);
-          });
-      });
-    },
+    // ArchiveProductionPlan(request) {
+    //   return new Promise((resolve, reject) => {
+    //     api
+    //       .put(
+    //         `cig-production/production-plan/production-planning/archive/${request.id}`,
+    //         request.payload
+    //       )
+    //       .then((response) => {
+    //         resolve(response.data);
+    //       })
+    //       .catch((error) => {
+    //         reject(error.response.data);
+    //       });
+    //   });
+    // },
 
-    EditProductionPlanStatus(request) {
-      return new Promise((resolve, reject) => {
-        api
-          .put(
-            `cig-production/production-plan/production-planning/edit-status/${request.id}`,
-            request.payload
-          )
-          .then((response) => {
-            resolve(response.data);
-          })
-          .catch((error) => {
-            reject(error.response.data);
-          });
-      });
-    },
+    // EditProductionPlanStatus(request) {
+    //   return new Promise((resolve, reject) => {
+    //     api
+    //       .put(
+    //         `cig-production/production-plan/production-planning/edit-status/${request.id}`,
+    //         request.payload
+    //       )
+    //       .then((response) => {
+    //         resolve(response.data);
+    //       })
+    //       .catch((error) => {
+    //         reject(error.response.data);
+    //       });
+    //   });
+    // },
   },
 });
